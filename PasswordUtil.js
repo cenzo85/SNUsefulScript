3b0d3f30bd41cde30e0b1f889041839597e6128f
Vincenzo <cenzo85@live.it>
2022-10-19 11:26:31 +0200

    Update PasswordUtil.js

--- PasswordUtil.js
+++ PasswordUtil.js
@@ -1,8 +1,8 @@
-
 var Encrypter = new GlideEncrypter();
-var user_name='ws_user_remedy';
-var utente = new GlideRecord('sys_user');
-if (utente.get('user_name',user_name)){
-	var password = Encrypter.decrypt(utente.user_password);
-	gs.print(utente.user_name+'--'+password);
+var gr = new GlideRecord('sys_auth_profile_basic');
+gr.query();
+while (gr.next()) {
+	var encrypted = gr.password;
+	var decrypted = Encrypter.decrypt(encrypted);  
+  gs.print(gr.username+' --> '+decrypted);
 }
